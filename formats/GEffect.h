991bd7c8bf674e44b881f935e757e217c29b05a3
Jiju Paul <mindstreamx@gmail.com>
2014-10-26 22:01:35 +0530

    Adding GEffect class for handling str files

---
+++ formats/GEffect.h
@@ -0,0 +1,51 @@
+#ifndef _FORMATS_GEFFECT_H
+#define _FORMATS_GEFFECT_H
+#include <vector>
+#include <fstream>
+#include <malloc.h>
+
+class GEffect//For Handling STR files used for creating Effects
+{
+    public:
+        struct Frame //Animation KeyFrame
+        {
+            uint32_t dwFrameNum;
+            uint32_t dwFrameType;
+            float fX;
+            float fY;
+            float fU;
+            float fV;
+            float fUS;
+            float fVS;
+            float fU2;
+            float fV2;
+            float fUS2;
+            float fVS2;
+            float fAX[4];//Vertice coordinates
+            float fAY[4];//Vertice coordinates
+            float fAniFrame;
+            uint32_t dwAniType;
+            float fAniDelta;
+            float fRotation;
+            uint32_t dwRGBA;//should be converted to color
+            uint32_t dwSrcBlend;
+            uint32_t dwDstBlend;
+            uint32_t dwMtPreset;
+        };
+
+        struct Layer
+        {
+            std::vector<char*> vImages;
+            std::vector<GEffect::Frame*> vFrames;
+        };
+
+        GEffect(const char* sFile);
+        virtual ~GEffect();
+
+    private:
+        uint32_t dwVersion;
+        std::vector<Layer*> vLayers;
+        void fetchLayer(std::fstream* pFile, GEffect::Layer* pLayer);
+};
+
+#endif//_FORMATS_GEFFECT_H
